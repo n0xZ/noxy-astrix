7dd5a1dc262038fc2f6d521da9dbf1afbf3493ec
n0xZ <o.gonzalo@hotmail.com>
2022-11-19 20:55:39 -0300

    setup astro image integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
-import { defineConfig } from 'astro/config'
-import Unocss from 'unocss/astro'
+import { defineConfig } from 'astro/config';
+import Unocss from 'unocss/astro';
 // https://astro.build/config
-export default defineConfig({ integrations: [Unocss()] })
+import image from "@astrojs/image";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [Unocss(), image()]
+});
